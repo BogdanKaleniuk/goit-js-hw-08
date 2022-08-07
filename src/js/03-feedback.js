394acf9e2b60dd21abab56af7a981f3cb23c56a1
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-08-07 18:50:33 +0300

    start 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,57 @@
-import throttle from 'lodash.throttle';
-const STORAGE_KEY = 'feedback-form-state';
+// import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-form.addEventListener('input', throttle(onFormData, 500));
-form.addEventListener('submit', onSubmitForm);
+// const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {};
+// const form = document.querySelector('.feedback-form');
+// form.addEventListener('input', throttle(onFormData, 500));
+// form.addEventListener('submit', onSubmitForm);
 
-function onFormData(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+// let formData = {};
+
+// function onFormData(e) {
+//   formData[e.target.name] = e.target.value;
+//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+//   console.log(e.target.value);
+// }
+
+// function onSubmitForm(e) {
+//   e.preventDefault();
+
+//   const email = document.querySelector('.feedback-form input');
+//   const message = document.querySelector('.feedback-form textarea');
+//   if (message.value === '' || email.value === '') {
+//     alert(`Всі поля мають бути заповнені!`);
+//   } else {
+//     e.currentTarget.reset();
+//     localStorage.removeItem(STORAGE_KEY);
+
+//     console.log(formData);
+//   }
+// }
+
+// (function updateDataFromLocalStorage() {
+//   let data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+//   if (data) {
+//     Object.entries(data).forEach(([key, value]) => {
+//       formData[key] = value;
+//       form.elements[key].value = value;
+//       console.log(data);
+//     });
+//   }
+// })();
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.textarea'),
+};
+
+refs.form.addEventListener('input', onFormSubmit);
+refs.textarea.addEventListener('submit', onTextareaSubmit);
+
+function onFormSubmit(e) {}
+
+function onTextareaSubmit(e) {
+  const value = e.currentTarget.value;
+  console.log(value);
 }
-
-function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
-
-  e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
-}
-
-(function dataFromLocalStorage() {
-  let data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
-
-  if (data) {
-    email.value = data.email;
-    message.value = data.message;
-  }
-
-  console.log(data);
-})();
