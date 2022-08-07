65eecc874831d88ab86267edaf176b2bf10f7f1e
BogdanKaleniuk <98873903+BogdanKaleniuk@users.noreply.github.com>
2022-08-07 16:22:51 +0000

    Deploying to gh-pages from @ BogdanKaleniuk/goit-js-hw-08@47b4eac65c213c305f45a20d6c5f5355b044ece6 🚀

---
+++ 03-feedback.00c35432.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-form-state",t)}));
+//# sourceMappingURL=03-feedback.00c35432.js.map
--- 03-feedback.7c4715fb.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".textarea")};e.form.addEventListener("input",(function(e){})),e.textarea.addEventListener("submit",(function(e){const t=e.currentTarget.value;console.log(t)}));
-//# sourceMappingURL=03-feedback.7c4715fb.js.map
