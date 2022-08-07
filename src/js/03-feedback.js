5d076a7d4da52d8ecf3f649308c667f0c68a71bd
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-08-07 18:17:05 +0300

    done 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
 
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(onFormData, 500));
+form.addEventListener('submit', onSubmitForm);
+
+const formData = {};
+
+function onFormData(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onSubmitForm(e) {
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+
+  e.preventDefault();
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+(function dataFromLocalStorage() {
+  let data = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  const email = document.querySelector('.feedback-form input');
+  const message = document.querySelector('.feedback-form textarea');
+
+  if (data) {
+    email.value = data.email;
+    message.value = data.message;
+  }
+
+  console.log(data);
+})();
