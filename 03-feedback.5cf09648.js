c4b17940373ce6cc0efd3d332932371dc133e6df
BogdanKaleniuk <98873903+BogdanKaleniuk@users.noreply.github.com>
2022-08-07 15:51:07 +0000

    Deploying to gh-pages from @ BogdanKaleniuk/goit-js-hw-08@394acf9e2b60dd21abab56af7a981f3cb23c56a1 🚀

---
+++ 03-feedback.5cf09648.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".textarea")};e.form.addEventListener("input",(function(e){})),e.textarea.addEventListener("submit",(function(e){var t=e.currentTarget.value;console.log(t)}))}();
+//# sourceMappingURL=03-feedback.5cf09648.js.map
