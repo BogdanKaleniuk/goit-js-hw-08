effeb38af5a2cfb9b217e93bb4b30cb4aa4ce03a
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2022-08-08 11:52:50 +0300

    done 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,72 +1,61 @@
-// import throttle from 'lodash.throttle';
-
-// const STORAGE_KEY = 'feedback-form-state';
-
-// const form = document.querySelector('.feedback-form');
-// form.addEventListener('input', throttle(onFormData, 500));
-// form.addEventListener('submit', onSubmitForm);
-
-// let formData = {};
-
-// function onFormData(e) {
-//   formData[e.target.name] = e.target.value;
-//   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-//   console.log(e.target.value);
-// }
-
-// function onSubmitForm(e) {
-//   e.preventDefault();
-
-//   const email = document.querySelector('.feedback-form input');
-//   const message = document.querySelector('.feedback-form textarea');
-//   if (message.value === '' || email.value === '') {
-//     alert(`Всі поля мають бути заповнені!`);
-//   } else {
-//     e.currentTarget.reset();
-//     localStorage.removeItem(STORAGE_KEY);
-
-//     console.log(formData);
-//   }
-// }
-
-// (function updateDataFromLocalStorage() {
-//   let data = JSON.parse(localStorage.getItem(STORAGE_KEY));
-
-//   if (data) {
-//     Object.entries(data).forEach(([key, value]) => {
-//       formData[key] = value;
-//       form.elements[key].value = value;
-//       console.log(data);
-//     });
-//   }
-// })();
 import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formData = {};
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   textarea: document.querySelector('.textarea'),
+// };
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.textarea'),
-};
-
-refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+form.addEventListener('input', throttle(onTextareaInput, 1000));
+form.addEventListener('submit', onFormSubmit);
 
 populateTextarea();
 
-function onFormSubmit(e) {
-  e.preventDefault();
-
-  console.log('Отправляем форму');
-
-  e.currentTarget.reset();
+function onTextareaInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function onTextareaInput(e) {
-  const massage = e.currentTarget.value;
-  // console.log(massage);
-  localStorage.setItem('feedback-form-state', massage);
+// const onTextareaInput = {};
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  const email = document.querySelector('input');
+  const message = document.querySelector('textarea');
+  if (email.value === '' || message.value === '') {
+    alert(`Всі поля мають бути заповнені!`);
+  } else {
+    // e.currentTarget.reset();
+    form.reset();
+
+  // console.log('Отправляем форму');
+  
+  localStorage.removeItem(STORAGE_KEY);
+  console.log(formData);
 }
 
 function populateTextarea() {
-  const savedMessage = localStorage.getItem('feedback-form-state;');
-  console.log(savedMessage);
+  let data = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+
+  if (data) {
+    Object.entries(saveData).forEach(([key, value]) => {
+      formData[key] = value;
+      form.elements[key].value = value;
+      console.log(saveData);
+    });
+  }
 }
+// function onTextareaInput(e) {
+//   e.preventDefault();
+//   const massage = e.target.value;
+//   localStorage.setItem(STORAGE_KEY, massage);
+// }
+
+// function populateTextarea() {
+//   e.preventDefault();
+//   const savedMessage = localStorage.getItem(STORAGE_KEY);
+//   if (savedMessage) {
+//     refs.textarea.value = savedMessage;
+//   }
+// }
